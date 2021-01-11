8b983311eb971531da50803c601709fafb18d7b6
Ricardo Corrêa <r.c.correa@outlook.com>
2021-01-11 16:33:20 -0300

    :hammer: configure release for Docker Hub

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,10 +1,10 @@
-VERSION=0.0.2-test
+VERSION=0.0.1
 CONTAINER_NAMESPACE=ricardocorrea
-SOFTWARE=connect-database
+SOFTWARE=database-connection-test
 
 docker build -f ./scripts/Dockerfile -t $CONTAINER_NAMESPACE/$SOFTWARE:$VERSION .
 
-# docker push $CONTAINER_NAMESPACE/$SOFTWARE:$VERSION
+docker push $CONTAINER_NAMESPACE/$SOFTWARE:$VERSION
 
 echo "Finished. Press a key to exit..."
 read -s -n 1 key
